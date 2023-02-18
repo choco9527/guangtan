2924c5ad87e3e774780f73fd385feb39638390a3
wuzk <wuzk@flatincbr.com>
2023-02-19 00:45:57 +0800

    tabbar

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -4,6 +4,16 @@ Page({
   },
   onChange(event) {
     // event.detail 的值为当前选中项的索引
-    this.setData({ active: event.detail });
+    const active = event.detail
+    this.setData({active});
+    const list = [
+      "/pages/index/index",
+      "/pages/searchList/index",
+      '/pages/userPage/index'
+    ]
+    console.log(list[active]);
+    wx.switchTab({
+      url: list[active]
+    })
   },
 });
